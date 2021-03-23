88c4759e2d0bdf9d818871e384de35782abbc18c
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-23 09:26:06 +0100

    fixes
    fixed tags and touchups to cards

--- js/lib/card.js
+++ js/lib/card.js
@@ -1,26 +1,21 @@
 export function createCard(data) {
   const cardSection = document.createElement('div')
-  cardSection.classList.add('question-box')
-  cardSection.classList.add('content-box')
+  cardSection.classList.add('question-box', 'content-box')
 
   const h2Title = document.createElement('h2')
-  h2Title.classList.add('question-box__title')
-  h2Title.classList.add('content-header')
+  h2Title.classList.add('question-box__title', 'content-header')
   h2Title.textContent = data.id
 
   cardSection.append(h2Title)
 
   const pElement = document.createElement('p')
-  pElement.classList.add('question-box__paragrgaph')
-  pElement.classList.add('content-text')
+  pElement.classList.add('question-box__paragrgaph', 'content-text')
   pElement.textContent = data.question
 
   cardSection.append(pElement)
 
   const iElement = document.createElement('i')
-  iElement.classList.add('sample-bookmark')
-  iElement.classList.add('fas')
-  iElement.classList.add('fa-bookmark')
+  iElement.classList.add('sample-bookmark', 'fas', 'fa-bookmark')
   if (data.isBookmarked) {
     iElement.classList.add('js-bookmark')
   }
@@ -44,45 +39,26 @@ export function createCard(data) {
 
   cardSection.append(button)
 
-  const answer = document.createElement('answer')
-  answer.classList.add('hidden')
-  answer.classList.add('answer')
+  const answer = document.createElement('p')
+  answer.classList.add('hidden', 'answer')
   answer.textContent = data.answer
 
   cardSection.append(answer)
 
   const ulElement = document.createElement('ul')
-  ulElement.classList.add('ul-flex')
-  ulElement.classList.add('content-topics')
+  ulElement.classList.add('ul-flex', 'content-topics')
 
   cardSection.append(ulElement)
 
   for (let i = 0; i < data.tags.length; i++) {
     const listItemElement = document.createElement('li')
     listItemElement.textContent = data.tags[i]
+    listItemElement.style.width = '60px'
+    listItemElement.style.alignSelf = 'center'
+    listItemElement.style.textAlign = 'center'
+    listItemElement.style.padding = '2px'
     ulElement.append(listItemElement)
   }
 
   return cardSection
 }
-
-/* 
-        <div class="question-box content-box">
-          <h2 class="question-box__title content-header">Question</h2>
-          <p class="question-box__paragrgaph content-text">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos,
-            fugiat?
-          </p>
-
-          <i class="js-bookmark sample-bookmark fas fa-bookmark"></i>
-
-          <button type="button" class="answer-button">Answer</button>
-          <p class="hidden answer">This answer is...</p>
-
-          <ul class="ul-flex content-topics">
-            <li>#Loram</li>
-            <li>#Loram</li>
-            <li>#Loram</li>
-          </ul>
-        </div>
-       */
